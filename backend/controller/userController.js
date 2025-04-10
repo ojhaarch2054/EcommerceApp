619ec7db6fb61a65ec168e22e3a430f7f787d484
Archana Ojha <t3ojar01@students.oamk.fi>
2025-04-10 17:07:20 +0300

    added bcrypt to hash the password

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const db = require("../models/db");
+const bcrypt = require("bcrypt");
 
 //to get user
 const getUsers = async (req, res) => {
@@ -14,22 +15,22 @@ const getUsers = async (req, res) => {
 
 //to post user for sign up
 const postUserSignUp = async (req, res) => {
+  const saltRounds = 10;
   try {
     const { name, email, phone_number, address, password } = req.body;
-
     //validate input
     if (!name || !email || !password) {
-        return res
+      return res
         .status(400)
         .json({ error: "name, email, and password are required" });
     }
-
+    //hash the password
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
     //add new user into the DB
     const result = await db.query(
-        "INSERT INTO users (name, email, phone_number, address, password) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-        [name, email, phone_number, address, password]
+      "INSERT INTO users (name, email, phone_number, address, password) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [name, email, phone_number, address, hashedPassword]
     );
-
     //return newly created user
     res.status(201).json(result.rows[0]);
   } catch (error) {
