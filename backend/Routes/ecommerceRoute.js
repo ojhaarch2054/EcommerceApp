5aace2965150bb7e7b3af5e0366d712a65a46a20
Archana Ojha <t3ojar01@students.oamk.fi>
2025-04-11 08:22:05 +0300

    added login functionality in backend

--- backend/Routes/ecommerceRoute.js
+++ backend/Routes/ecommerceRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { getUsers, postUserSignUp} = require('../controller/userController');
+const { getUsers, postUserSignUp, postUserLogIn} = require('../controller/userController');
 
 const router = express.Router();
 
 //for login and signup
 router.get('/users', getUsers);
 router.post('/register_users', postUserSignUp);
+router.post('/logIn_users', postUserLogIn);
+
 
 module.exports = router;
--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,6 +1,15 @@
 const axios = require("axios");
-const db = require("../models/db");
+const pool = require("../models/db");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+dotenv.configDotenv();
+
+//fun to create a jwt token with the user id
+const createToken = (id, role) => {
+  return jwt.sign({ id, role }, process.env.secretKey, { expiresIn: "15m" });
+};
 
 //to get user
 const getUsers = async (req, res) => {
@@ -17,7 +26,16 @@ const getUsers = async (req, res) => {
 const postUserSignUp = async (req, res) => {
   const saltRounds = 10;
   try {
-    const { name, email, phone_number, address, password } = req.body;
+    const {
+      name,
+      email,
+      phone_number,
+      address,
+      password,
+      role = "admin",
+    } = req.body;
+    console.log("Received role:", role);
+
     //validate input
     if (!name || !email || !password) {
       return res
@@ -28,9 +46,10 @@ const postUserSignUp = async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, saltRounds);
     //add new user into the DB
     const result = await db.query(
-      "INSERT INTO users (name, email, phone_number, address, password) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-      [name, email, phone_number, address, hashedPassword]
+      "INSERT INTO users (name, email, phone_number, address, password, role) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      [name, email, phone_number, address, hashedPassword, role]
     );
+    console.log("added to table: " + result);
     //return newly created user
     res.status(201).json(result.rows[0]);
   } catch (error) {
@@ -39,4 +58,40 @@ const postUserSignUp = async (req, res) => {
   }
 };
 
-module.exports = { getUsers, postUserSignUp };
+//to login
+const postUserLogIn = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const result = await pool.query("select * from users where email = $1", [
+      email,
+    ]);
+    if (result.rows.length === 0) {
+      return res.status(400).json({ error: "Invalid email or password" });
+    }
+    const user = result.rows[0];
+    //compare pw with hashed
+    const samePassword = await bcrypt.compare(password, user.password);
+    if (!samePassword) {
+      return res.status(400).json({ error: "Invalid email or password" });
+    }
+    //create a token for the user
+    const token = createToken(user.id, user.role);
+    //set the token as a cookie
+    res.cookie("jwt", token, { httpOnly: true, maxAge: 15 * 60 * 1000 });
+
+    console.log("JWT token set:", token);
+    //send the user data and roles in the response
+    res.status(200).json({
+      id: user.id,
+      token: token,
+      user: user.email,
+      roles: user.role,
+    });
+    console.log(res.status);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+};
+
+module.exports = { getUsers, postUserSignUp, postUserLogIn };
