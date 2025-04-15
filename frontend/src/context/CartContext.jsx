b26dd416ec8f2b20152b3cc8fc5a51567f7ecc76
Archana Ojha <t3ojar01@students.oamk.fi>
2025-04-15 22:07:28 +0300

    added functionality to post  data in product table from frontend

--- frontend/src/context/CartContext.jsx
+++ frontend/src/context/CartContext.jsx
@@ -1,4 +1,5 @@
 import React, { createContext, useState } from "react";
+import axios from "axios";
 
 //create the context
 export const CartContext = createContext();
@@ -8,8 +9,20 @@ export const CartProvider = ({ children }) => {
   const [cartItem, setCartItem] = useState([]);
 
   //function to add an item to the cart
-  const addToCart = (product) => {
-    setCartItem((prevCart) => [...prevCart, product]);
+  const addToCart = async (product) => {
+    try {
+      //axios to send a post rqst to backend
+      const response = await axios.post("http://localhost:3000/add_product", product);
+  
+      //extract the newly added product from the response
+      const newProduct = response.data;
+      console.log("Product added to the database:", newProduct);
+  
+      //update the local cart state
+      setCartItem((prevCart) => [...prevCart, newProduct]);
+    } catch (error) {
+      console.error("Error adding product to cart:", error.response?.data || error.message);
+    }
   };
 
   return (
