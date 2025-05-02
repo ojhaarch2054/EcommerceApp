bd3ba25b8daa7b775aaeb225b309099bbad79fe1
Archana Ojha <t3ojar01@students.oamk.fi>
2025-05-02 12:22:06 +0300

    added condition for authenticated and non authenticated user for profile page

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,9 +1,34 @@
+import useAuth from "../context/Hook/useAuth";
+import { useNavigate } from "react-router-dom";
+
 const Profile = () => {
-    return (
-        <>
-            <h1>This is profile section....</h1>
-        </>
-    );
+  const { isAuthenticate } = useAuth();
+  const navigate = useNavigate()
+
+  const logInbtnn = () => {
+    navigate("/login")
+  }
+
+  return (
+    <>
+     <div className="container mt-5">
+      {isAuthenticate ? (
+        <div className="text-center">
+          <h1 className="display-4">This is your profile section</h1>
+        </div>
+      ) : (
+        <div className="text-center">
+          <h1 className="display-5 mb-4">
+            You need to log in to view your profile.
+          </h1>
+          <button className="btn btn-secondary btn-lg" onClick={logInbtnn}>
+            Log In
+          </button>
+        </div>
+      )}
+    </div>
+    </>
+  );
 };
 
 export default Profile;
